ed5c239b85342c0d32bb813d1dde55ae593091df
Talha Imran <talhaimran284@gmail.com>
2023-11-16 13:57:13 +0500

    Updated configuration for null-ls

--- lua/talhaimran/null-ls.lua
+++ lua/talhaimran/null-ls.lua
@@ -1,3 +1,14 @@
+require("mason").setup()
+
 require("mason-null-ls").setup({
-  handlers = {},
+    ensure_installed = {
+        "prettier"
+    },
+    automatic_installation = true
+})
+
+require("null-ls").setup({
+    sources = {
+        require("null-ls").builtins.formatting.prettier
+    }
 })
