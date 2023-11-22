34ea891666d79e2f0d86c110379c90b02b4cdc80
Talha Imran <talhaimran284@gmail.com>
2023-11-22 22:32:01 +0500

    Updated key bindings for telescope.

--- lua/talhaimran/telescope.lua
+++ lua/talhaimran/telescope.lua
@@ -2,8 +2,8 @@ require('telescope').setup {
   defaults = {
     mappings = {
       i = {
-        ["<A-j>"] = require('telescope.actions').move_selection_next,
-        ["<A-k>"] = require('telescope.actions').move_selection_previous,
+        ["<S-j>"] = require('telescope.actions').move_selection_next,
+        ["<S-k>"] = require('telescope.actions').move_selection_previous,
         ["<C-j>"] = require('telescope.actions').cycle_history_next,
         ["<C-k>"] = require('telescope.actions').cycle_history_prev
       }
