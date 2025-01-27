bafa60522bcfd4374723657ea1415a2cf4270ce5
Talha Imran <talhaimran284@gmail.com>
2025-01-27 19:54:54 +0500

    update

--- lua/talhaimran/keymaps.lua
+++ lua/talhaimran/keymaps.lua
@@ -135,3 +135,14 @@ function OrganizeImports()
     arguments = { vim.fn.expand('%:p') },
   })
 end
+
+-- Create directories before writing a file / Needed for Avante.nvim, Remove this if you don't use Avante
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    local dir = vim.fn.expand("<afile>:p:h")
+    if vim.fn.isdirectory(dir) == 0 then
+      vim.fn.mkdir(dir, "p")
+    end
+  end,
+})
